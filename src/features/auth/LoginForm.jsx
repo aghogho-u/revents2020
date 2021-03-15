7cd70bd56fbba086076d46f18219684b0b676307
unknown <auwejeyan@gmail.com>
2021-03-15 03:52:04 +0100

    Login and logout

--- src/features/auth/LoginForm.jsx
+++ src/features/auth/LoginForm.jsx
@@ -5,8 +5,9 @@ import * as Yup from 'yup';
 import MyTextInput from '../../app/common/form/MyTextInput'
 import { Button } from 'semantic-ui-react';
 import { useDispatch } from 'react-redux';
-import { signInUser } from './authActions';
+//import { signInUser } from './authActions';
 import { closeModal } from '../../app/common/modals/modalReducer';
+import { signInWithEmail } from '../../app/firestore/firebaseService';
 
 export default function LoginForm(){
     const dispatch = useDispatch();
@@ -16,12 +17,18 @@ export default function LoginForm(){
                 initialValues={{email: '', password: ''}}
                 validationSchema={Yup.object({
                     email: Yup.string().required().email(),
-                    password: Yup.string().required
+                    password: Yup.string().required()
                 })}
-                onSubmit={(values, {setsubmitting})=>{
-                    dispatch(signInUser(values));
-                    setsubmitting(false);
+                onSubmit={async (values, {setSubmitting})=>{ 
+                    try{
+                    await dispatch(signInWithEmail(values));
+                    setSubmitting(false);
                     dispatch(closeModal());
+                    }catch(error){
+                        setSubmitting(false);
+                        console.log(error)
+                    }
+                    
                 }}
             >
                 {({isSubmitting, isValid, dirty})=>(
